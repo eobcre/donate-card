9ae6bed479b1e144b9aab7e481db757e86f3c18a
eobcre <eobcre@gmail.com>
2023-08-23 17:40:42 -0400

    placed components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import Title from './components/Title';
+import SelectTab from './components/SelectTab';
+import Price from './components/Price';
+import Input from './components/Input';
+import DonateButton from './components/DonateButton';
+import Terms from './components/Terms';
+
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Title />
+      <SelectTab />
+      <Price />
+      <Input />
+      <DonateButton />
+      <Terms />
+    </div>
+  );
 };
 
 export default App;

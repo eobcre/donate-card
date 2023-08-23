19222117ceac0a43ef0b5e39e16371b9880c8906
eobcre <eobcre@gmail.com>
2023-08-23 19:09:31 -0400

    title styled

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,5 +1,11 @@
 const Title = () => {
-  return <div>Title</div>;
+  return (
+    <div>
+      <h1 className='flex justify-center items-center text-[#5A9CFF] text-xl font-bold'>
+        Donate
+      </h1>
+    </div>
+  );
 };
 
 export default Title;

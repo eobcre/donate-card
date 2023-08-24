727a9010282dd2f2eb6e92c713faf541eff87298
eobcre <eobcre@gmail.com>
2023-08-23 20:03:04 -0400

    created data

---
+++ src/data/priceData.tsx
@@ -0,0 +1,32 @@
+export const priceData = [
+  {
+    id: 1,
+    price: 5,
+    checked: false,
+  },
+  {
+    id: 2,
+    price: 10,
+    checked: false,
+  },
+  {
+    id: 3,
+    price: 15,
+    checked: false,
+  },
+  {
+    id: 4,
+    price: 25,
+    checked: false,
+  },
+  {
+    id: 5,
+    price: 50,
+    checked: false,
+  },
+  {
+    id: 6,
+    price: 'Custom',
+    checked: false,
+  },
+];

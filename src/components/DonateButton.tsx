124da2250f3225423aa1b28c6b1bff367ff6ad5a
eobcre <eobcre@gmail.com>
2023-08-23 21:05:59 -0400

    created and styled button

--- src/components/DonateButton.tsx
+++ src/components/DonateButton.tsx
@@ -1,5 +1,11 @@
-const DonateButton = () => {
-  return <div>DonateButton</div>;
+const DonateButton: React.FC = () => {
+  return (
+    <div className='flex justify-center items-center'>
+      <button className='bg-[#5A9CFF] text-white text-sm rounded-md px-6 py-2'>
+        Donate Now
+      </button>
+    </div>
+  );
 };
 
 export default DonateButton;

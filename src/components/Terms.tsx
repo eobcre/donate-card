82c7a5f8ee959d8810c1814093fa974ffd320f98
eobcre <eobcre@gmail.com>
2023-08-23 21:06:47 -0400

    created and styled policy

--- src/components/Terms.tsx
+++ src/components/Terms.tsx
@@ -1,5 +1,18 @@
 const Terms = () => {
-  return <div>Terms</div>;
+  return (
+    <div>
+      <p className='text-center text-xs my-5'>
+        By donating, you agree to our{' '}
+        <a href='#' className='text-[#BAD1FF]'>
+          terms of service
+        </a>{' '}
+        and{' '}
+        <a href='#' className='text-[#BAD1FF]'>
+          privacy policy.
+        </a>
+      </p>
+    </div>
+  );
 };
 
 export default Terms;

7ae4f8d8b1537f8d32132c7f2f9f2ee9e9896b24
eobcre <eobcre@gmail.com>
2023-08-23 20:00:44 -0400

    created tab buttons and styled

--- src/components/SelectTab.tsx
+++ src/components/SelectTab.tsx
@@ -1,5 +1,12 @@
 const SelectTab = () => {
-  return <div>SelectTab</div>;
+  return (
+    <div className='flex justify-center items-center'>
+      <div className='flex justify-center items-center gap-16 bg-[#5A9CFF] rounded-sm w-[280px] h-[50px]'>
+        <input type='button' value='Monthly' />
+        <input type='button' value='One Time' />
+      </div>
+    </div>
+  );
 };
 
 export default SelectTab;

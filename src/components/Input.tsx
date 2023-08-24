c332c5730b5fe9190db3acd64340a0f0c046c1c5
eobcre <eobcre@gmail.com>
2023-08-23 21:45:56 -0400

    linked label and input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,19 +1,19 @@
-const Input = () => {
+const Input: React.FC = () => {
   return (
     <div className='flex justify-center items-center'>
       <div className='flex flex-col text-sm'>
-        <label htmlFor={`checkbox`}>Name</label>
+        <label htmlFor={`name-linked`}>Name</label>
         <input
           type='text'
-          id={`checkbox`}
+          id={`name-linked`}
           placeholder='Enter your name'
           className='border border-[#BAD1FF] rounded-md outline-none my-3 p-2 w-[360px]'
         />
 
-        <label htmlFor={`checkbox`}>Email</label>
+        <label htmlFor={`email-linked`}>Email</label>
         <input
           type='text'
-          id={`checkbox`}
+          id={`email-linked`}
           placeholder='Enter your email'
           className='border border-[#BAD1FF] outline-none rounded-md my-3 p-2'
         />

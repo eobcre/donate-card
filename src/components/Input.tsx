f40ac3fcf1862d5567fc8c2f0210a8cd3c04ebb9
eobcre <eobcre@gmail.com>
2023-08-23 21:04:19 -0400

    created name, email input and styled

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,5 +1,25 @@
 const Input = () => {
-  return <div>Input</div>;
+  return (
+    <div className='flex justify-center items-center'>
+      <div className='flex flex-col text-sm'>
+        <label htmlFor={`checkbox`}>Name</label>
+        <input
+          type='text'
+          id={`checkbox`}
+          placeholder='Enter your name'
+          className='border border-[#BAD1FF] rounded-md outline-none my-3 p-2 w-[360px]'
+        />
+
+        <label htmlFor={`checkbox`}>Email</label>
+        <input
+          type='text'
+          id={`checkbox`}
+          placeholder='Enter your email'
+          className='border border-[#BAD1FF] outline-none rounded-md my-3 p-2'
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Input;

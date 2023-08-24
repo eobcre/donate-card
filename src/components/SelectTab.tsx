618bfad2db2b7b384a714df2df28bed96141503c
eobcre <eobcre@gmail.com>
2023-08-23 21:05:26 -0400

    updated text color, size and height

--- src/components/SelectTab.tsx
+++ src/components/SelectTab.tsx
@@ -1,7 +1,7 @@
 const SelectTab = () => {
   return (
     <div className='flex justify-center items-center'>
-      <div className='flex justify-center items-center gap-16 bg-[#5A9CFF] rounded-sm w-[280px] h-[50px]'>
+      <div className='flex justify-center items-center gap-16 bg-[#5A9CFF] text-white text-sm rounded-md w-[280px] h-[48px]'>
         <input type='button' value='Monthly' />
         <input type='button' value='One Time' />
       </div>

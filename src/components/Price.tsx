430795031a76c87602b732314f0f9b462e6883bc
eobcre <eobcre@gmail.com>
2023-08-23 21:04:51 -0400

    updated margin, text size

--- src/components/Price.tsx
+++ src/components/Price.tsx
@@ -2,19 +2,19 @@ import { priceData } from '../data/priceData';
 
 const Price: React.FC = () => {
   return (
-    <div className='flex justify-center items-center gap-2 my-6'>
+    <div className='flex justify-center items-center gap-2 my-5'>
       {priceData.map((data) => (
         <input
           key={data.id}
           type='button'
           value={`$${data.price}`}
-          className='border border-[#BAD1FF] rounded-md w-[50px] py-1'
+          className='border border-[#BAD1FF] text-sm rounded-md w-[50px] py-1'
         />
       ))}
       <input
         type='button'
         value='Custom'
-        className='border border-[#BAD1FF] rounded-md px-2 py-1'
+        className='border border-[#BAD1FF] text-sm rounded-md px-2 py-1'
       />
     </div>
   );

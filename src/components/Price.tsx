0b17ff342e87cbb7f40ba3bbb36f4b04e1da7560
eobcre <eobcre@gmail.com>
2023-08-23 20:02:53 -0400

    created buttons looping logic

--- src/components/Price.tsx
+++ src/components/Price.tsx
@@ -1,5 +1,13 @@
-const Price = () => {
-  return <div>Price</div>;
+import { priceData } from '../data/priceData';
+
+const Price: React.FC = () => {
+  return (
+    <div>
+      {priceData.map((data) => (
+        <input key={data.id} type='button' value={data.price} />
+      ))}
+    </div>
+  );
 };
 
 export default Price;

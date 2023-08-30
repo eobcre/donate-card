7bc90b4b2c8e927196b1940d8716a1823def5655
eobcre <eobcre@gmail.com>
2023-08-30 14:09:40 -0400

    select price button with background color logic

--- src/components/Price.tsx
+++ src/components/Price.tsx
@@ -1,20 +1,34 @@
+import { useState } from 'react';
+
 import { priceData } from '../data/priceData';
 
 const Price: React.FC = () => {
+  const [selected, setSelected] = useState<number | null>(null);
+
+  const handleClick = (index: number | null) => {
+    setSelected(index);
+  };
+
   return (
     <div className='flex justify-center items-center gap-2 my-5'>
-      {priceData.map((data) => (
+      {priceData.map((data, index) => (
         <input
           key={data.id}
           type='button'
           value={`$${data.price}`}
-          className='border border-[#BAD1FF] text-sm rounded-md w-[50px] py-1'
+          onClick={() => handleClick(index)}
+          className={`border border-[#BAD1FF] text-sm rounded-md w-[50px] py-1 ${
+            selected === index ? 'bg-[#BAD1FF]' : ''
+          }`}
         />
       ))}
       <input
         type='button'
         value='Custom'
-        className='border border-[#BAD1FF] text-sm rounded-md px-2 py-1'
+        onClick={() => handleClick(null)}
+        className={`border border-[#BAD1FF] text-sm rounded-md w-[70px] py-1 ${
+          selected === null ? 'bg-[#BAD1FF]' : ''
+        }`}
       />
     </div>
   );

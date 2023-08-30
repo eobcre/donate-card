b407de43b301f85ef5ec0eb6b0281a75a6d48171
eobcre <eobcre@gmail.com>
2023-08-30 14:10:39 -0400

    created new component and placed components

---
+++ src/components/MonthlyTab.tsx
@@ -0,0 +1,15 @@
+import Price from './Price';
+import Input from './Input';
+import DonateButton from './DonateButton';
+
+const MonthlyTab = () => {
+  return (
+    <div>
+      <Price />
+      <Input />
+      <DonateButton />
+    </div>
+  );
+};
+
+export default MonthlyTab;

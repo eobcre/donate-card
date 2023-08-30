91018eb4d564afd5c2cea9dc5e5bf3456f4e2186
eobcre <eobcre@gmail.com>
2023-08-30 18:52:59 -0400

    created new component

---
+++ src/components/OneTimeTab.tsx
@@ -0,0 +1,15 @@
+import Price from './Price';
+import Input from './Input';
+import DonateButton from './DonateButton';
+
+const OneTimeTab: React.FC = () => {
+  return (
+    <div>
+      <Price />
+      <Input />
+      <DonateButton />
+    </div>
+  );
+};
+
+export default OneTimeTab;

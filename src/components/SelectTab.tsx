4974b9df2e8f5a89763cad447040c5be6204fede
eobcre <eobcre@gmail.com>
2023-08-29 20:38:19 -0400

    created useState for toggle tab

--- src/components/SelectTab.tsx
+++ src/components/SelectTab.tsx
@@ -1,4 +1,8 @@
+import { useState } from 'react';
+
 const SelectTab = () => {
+  const [changeTab, setChangeTab] = useState(false);
+
   return (
     <div className='flex justify-center items-center'>
       <div className='flex justify-center items-center gap-16 bg-[#5A9CFF] text-white text-sm rounded-md w-[280px] h-[48px]'>

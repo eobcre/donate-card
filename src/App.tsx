7e83ca22d8fd215ff42bff1ac1184c6d73e0802e
eobcre <eobcre@gmail.com>
2023-08-30 20:02:10 -0400

    moved state and handleClick to App, passed props to SelectTab and component toggle logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
+import { useState } from 'react';
+
 import Title from './components/Title';
 import SelectTab from './components/SelectTab';
 import Terms from './components/Terms';
 import MonthlyTab from './components/MonthlyTab';
+import OneTimeTab from './components/OneTimeTab';
 
 const App: React.FC = () => {
+  const [changeTab, setChangeTab] = useState<'Monthly' | 'One Time'>('Monthly');
+
+  const handleClick = (tab: 'Monthly' | 'One Time') => {
+    setChangeTab(tab);
+  };
+
   return (
     <div className='flex justify-center items-center bg-[#E4EFFF] h-screen'>
       <div className='bg-white rounded-xl w-[430px] h-[460px]'>
         <Title />
-        <SelectTab />
-        <MonthlyTab />
+        <SelectTab changeTab={changeTab} handleClick={handleClick} />
+        {changeTab === 'Monthly' ? <MonthlyTab /> : <OneTimeTab />}
         <Terms />
       </div>
     </div>

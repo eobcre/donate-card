8fb5995a2081d8090427ffba7dcb29db402d2da8
eobcre <eobcre@gmail.com>
2023-09-08 12:25:11 -0400

    props type and props settings

--- src/components/DonateButton.tsx
+++ src/components/DonateButton.tsx
@@ -1,8 +1,12 @@
-const DonateButton: React.FC = () => {
+type ButtonProps = {
+  name: string;
+};
+
+const DonateButton: React.FC<ButtonProps> = ({ name }) => {
   return (
     <div className='flex justify-center items-center'>
       <button className='bg-[#5A9CFF] text-white text-sm rounded-md px-6 py-2'>
-        Donate Now
+        {name}
       </button>
     </div>
   );
